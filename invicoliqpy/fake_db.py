d5358c6b411bf79d36f936291cf0479008f23035
Fernando Corrales <fscorrales@gmail.com>
2022-04-28 11:35:57 -0300

    fix: fake_factureros definition

--- invicoliqpy/fake_db.py
+++ invicoliqpy/fake_db.py
@@ -5,23 +5,25 @@ import sys
 from faker import Faker
 
 
-def create_fake_factureros(n):
+def fake_factureros(n):
     """Generate fake users."""
     faker = Faker('es_ES')
+    actividad = ['01-00-01', '01-00-02', '01-00-03', 
+    '01-00-04', '11-00-01', '12-00-01']
+
     for i in range(n):
         facturero = Factureros(
                     nombre_completo=faker.name(),
-                    actividad=random.randint(100, 500),
+                    actividad=random.choice(actividad),
                     partida=random.randint(300, 399)
                     )
         db.session.add(facturero)
     db.session.commit()
     print(f'Added {n} fake factureros to the database.')
 
-
-if __name__ == '__main__':
+""" if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print('Pass the number of factureros you want to create as an argument.')
         sys.exit(1)
-    create_fake_factureros(int(sys.argv[1]))
+    fake_factureros(int(sys.argv[1])) """
 
