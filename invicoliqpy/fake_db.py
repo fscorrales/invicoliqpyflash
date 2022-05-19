89883297f1853fa962cbb61310ba4ac976308899
Fernando Corrales <fscorrales@gmail.com>
2022-05-19 12:55:24 -0300

    feat: add honorarios factureros model

--- invicoliqpy/fake_db.py
+++ invicoliqpy/fake_db.py
@@ -1,16 +1,17 @@
 from invicoliqpy import db
-from invicoliqpy.models import Factureros
+from invicoliqpy.models import Factureros, HonorariosFactureros
 import random
 import sys
 from faker import Faker
+from datetime import datetime
 
+
+faker = Faker('es_ES')
+actividad = ['01-00-01', '01-00-02', '01-00-03', 
+'01-00-04', '11-00-01', '12-00-01']
 
 def fake_factureros(n):
     """Generate fake users."""
-    faker = Faker('es_ES')
-    actividad = ['01-00-01', '01-00-02', '01-00-03', 
-    '01-00-04', '11-00-01', '12-00-01']
-
     for i in range(n):
         facturero = Factureros(
                     nombre_completo=faker.name(),
@@ -21,9 +22,22 @@ def fake_factureros(n):
     db.session.commit()
     print(f'Added {n} fake factureros to the database.')
 
+def fake_honorarios_factureros(n):
+    """Generate fake Honorarios Factureros."""
+    for i in range(n):
+        honorario = HonorariosFactureros(
+                    fecha = datetime.utcnow(),
+                    facturero=faker.name(),
+                    importe_bruto=random.randint(10000, 100000),
+                    actividad=random.choice(actividad),
+                    partida=random.randint(300, 399)
+                    )
+        db.session.add(honorario)
+    db.session.commit()
+    print(f'Added {n} fake honorarios factureros to the database.')
+
 """ if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print('Pass the number of factureros you want to create as an argument.')
         sys.exit(1)
     fake_factureros(int(sys.argv[1])) """
-
--- invicoliqpy/models.py
+++ invicoliqpy/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from invicoliqpy import db
 
 class Factureros(db.Model):
@@ -20,5 +21,50 @@ class Factureros(db.Model):
         return (
             f'ID: {self.id},'
             f'Nombre Completo: {self.nombre_completo}, '
+            f'Estructura: {self.actividad}-{self.partida}'
+        )
+
+class HonorariosFactureros(db.Model):
+    __tablename__ = 'honorarios_factureros'
+
+    id = db.Column(db.Integer, primary_key=True)
+    fecha = db.Column(db.Date)
+    facturero = db.Column(db.String(100))
+    sellos = db.Column(db.Float, default = 0)
+    seguro = db.Column(db.Float, default = 0)
+    nro_comprobante = db.Column(db.String(8))
+    tipo = db.Column(db.String(1), default = "H")
+    importe_bruto = db.Column(db.Float, default = 0)
+    iibb = db.Column(db.Float, default = 0)
+    libramiento_pago = db.Column(db.Float, default = 0)
+    otras_retenciones = db.Column(db.Float, default = 0)
+    anticipo = db.Column(db.Float, default = 0)
+    descuento = db.Column(db.Float, default = 0)
+    actividad = db.Column(db.String(8))
+    partida = db.Column(db.String(3))
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'fecha': self.fecha,
+            'facturero': self.facturero,
+            'sellos': self.sellos,
+            'seguro': self.seguro,
+            'nro_comprobante': self.nro_comprobante,
+            'tipo': self.tipo,
+            'importe_bruto': self.importe_bruto,
+            'iibb': self.iibb,
+            'libramiento_pago': self.libramiento_pago,
+            'otras_retenciones': self.otras_retenciones,
+            'anticipo': self.anticipo,
+            'descuento': self.descuento,
+            'actividad': self.actividad,
+            'partida': self.partida,
+        }
+
+    def __str__(self):
+        return (
+            f'ID: {self.id},'
+            f'Nro Comprobante: {self.nro_comprobante}, '
             f'Estructura: {self.actividad}-{self.partida}'
         )
