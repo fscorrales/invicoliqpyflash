82d367f8260e4e020b65a6181f2c5ccc9d68c65e
Fernando Corrales <corrales_fernando@hotmail.com>
2022-05-19 23:21:56 -0300

    fix: Change default method with ColumnDefault

--- invicoliqpy/models.py
+++ invicoliqpy/models.py
@@ -1,4 +1,4 @@
-from email.policy import default
+from sqlalchemy import ColumnDefault
 from invicoliqpy import db
 
 class Factureros(db.Model):
@@ -30,16 +30,16 @@ class HonorariosFactureros(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fecha = db.Column(db.Date)
     facturero = db.Column(db.String(100))
-    sellos = db.Column(db.Float, default = 0)
-    seguro = db.Column(db.Float, default = 0)
+    sellos = db.Column(db.Float, ColumnDefault(0))
+    seguro = db.Column(db.Float, ColumnDefault(0))
     nro_comprobante = db.Column(db.String(8))
-    tipo = db.Column(db.String(1), default = "H")
-    importe_bruto = db.Column(db.Float, default = 0)
-    iibb = db.Column(db.Float, default = 0)
-    libramiento_pago = db.Column(db.Float, default = 0)
-    otras_retenciones = db.Column(db.Float, default = 0)
-    anticipo = db.Column(db.Float, default = 0)
-    descuento = db.Column(db.Float, default = 0)
+    tipo = db.Column(db.String(1), ColumnDefault("H"))
+    importe_bruto = db.Column(db.Float, ColumnDefault(0))
+    iibb = db.Column(db.Float, ColumnDefault(0))
+    libramiento_pago = db.Column(db.Float, ColumnDefault(0))
+    otras_retenciones = db.Column(db.Float, ColumnDefault(0))
+    anticipo = db.Column(db.Float, ColumnDefault(0))
+    descuento = db.Column(db.Float, ColumnDefault(0))
     actividad = db.Column(db.String(8))
     partida = db.Column(db.String(3))
 
